9d7a11494255f0ee7fd8367622e6e8edd4436347
MiguelOrtiz <miguel.ortiz16@inacapmail.cl>
2023-09-13 15:13:22 -0400

    xampp integracion

---
+++ XAMPP/cgi-bin/cgi.cgi
@@ -0,0 +1,5 @@
+#!"C:\Users\auron\OneDrive\Escritorio\XAMPP\perl\bin\perl.exe"
+
+print "Content-Type: text/html\n\n";
+
+print "OK";
--- forms/contact.php
+++ forms/contact.php
@@ -1,19 +1,44 @@
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $recipient_email = "miguel.ortiz.q55@gmail.com"; // Cambia esto al correo de destino deseado
-    $subject = $_POST["subject"];
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    $headers = "From: $name <$email>" . "\r\n";
-    $headers .= "Reply-To: $email" . "\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
+require 'XAMPP/php/PHPMailer/src/Exception.php'; // Rutas de los archivos PHP, revisar que sean correctas
+require 'XAMPP/php/PHPMailer/src/PHPMailer.php';
+require 'XAMPP/php/PHPMailer/src/SMTP.php';
 
-    mail($recipient_email, $subject, $message, $headers);
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$asunto = $_POST["asunto"];
+$contenido = $_POST["contenido"];
+
+// Crear una nueva instancia de PHPMailer
+$mail = new PHPMailer(true);
+
+try {
+    // Configurar el servidor SMTP de Gmail
+    $mail->SMTPDebug = 0; // Desactivar el modo de depuración (cambiar a 2 para ver detalles, 0 para el modo usuario)
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'auronferro@gmail.com'; // Reemplaza con tu dirección de correo Gmail
+    $mail->Password = 'gbzi immb ggrw fiqb'; // Reemplaza con tu contraseña Gmail (Puede requerir contraseña especial para apps)
+                                             // Revisar la documentacion
+    $mail->SMTPSecure = 'tls'; // Puede ser 'ssl' o 'tls'
+    $mail->Port = 587; // Puerto SMTP para TLS, 587 es el valor por defecto e ideal
+
+    // Configurar remitente y destinatario
+    $mail->setFrom($correo, $nombre);
+    $mail->addAddress('auronferro@gmail.com'); // Reemplaza con la dirección de correo destino
+
+    // Contenido del correo
+    $mail->isHTML(true);
+    $mail->Subject = $asunto;
+    $mail->Body = $contenido;
+
+    // Enviar el correo
+    $mail->send();
+    echo "El formulario se ha enviado con éxito.";
+} catch (Exception $e) {
+    echo "Ha ocurrido un error al enviar el formulario: {$mail->ErrorInfo}";
 }
 ?>
-
-<!-- En este punto, puedes mostrar un mensaje de confirmación al usuario en la página -->
-<div class="sent-message">Your message has been sent. Thank you!</div>
